c5c8f6f002514b68dd6c99537b599e6a97e17347
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2023-08-07 19:46:51 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "ap-south-1"  # Change this to your desired AWS region
+}
+
+# Create an S3 bucket for hosting the static website
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "your-unique-bucket-name"  # Replace this with your desired bucket name
+  acl    = "public-read"              # Make the bucket content publicly readable
+
+  website {
+    index_document = "index.html"      # Specify the main entry point of your website
+    error_document = "error.html"      # Specify the error page for the website (optional)
+  }
+}
+
+# Fetch the index.html content from GitHub and upload it to the S3 bucket
+data "http" "index_html" {
+  url = "https://raw.githubusercontent.com/your-github-username/your-repo-name/main/index.html"
+}
+
+# Upload the HTML file to the S3 bucket
+resource "aws_s3_bucket_object" "website_object" {
+  bucket = aws_s3_bucket.website_bucket.id
+  key    = "index.html"
+  acl    = "public-read"
+  content_type = "text/html"
+  source = data.http.index_html.body
+}
+
+# Output the website URL
+output "website_url" {
+  value = aws_s3_bucket.website_bucket.website_endpoint
+}
