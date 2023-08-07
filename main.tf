7797da00f805337df411f03852a4f17947a34ce4
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2023-08-07 20:25:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ provider "aws" {
 # Create an S3 bucket for hosting the static website
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "demo-bucket"  # Replace this with your desired bucket name
-  acl    = "public-read"              # Make the bucket content publicly readable
+  aws_s3_bucket_acl    = "public-read"              # Make the bucket content publicly readable
 }
 
 # Configure the website hosting for the S3 bucket
 resource "aws_s3_bucket_website_configuration" "website_config" {
   bucket = aws_s3_bucket.website_bucket.bucket
-  index_document = "index.html"      # Specify the main entry point of your website
+ 
 }
 
 # Fetch the index.html content from GitHub and upload it to the S3 bucket
@@ -20,20 +20,12 @@ data "http" "index_html" {
 }
 
 # Upload the HTML files to the S3 bucket
-resource "aws_s3_bucket_object" "index_html_object" {
+resource "aws_s3_object" "index_html_object" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
   acl    = "public-read"
   content_type = "text/html"
-  source = data.http.index_html.body
-}
-
-resource "aws_s3_bucket_object" "error_html_object" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "error.html"
-  acl    = "public-read"
-  content_type = "text/html"
-  source = data.http.error_html.body
+  source = data.http.index_html.body_raw
 }
 
 # Output the website URL
